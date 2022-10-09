5a9f3cd8c46972e0f1f41bdb5ed8c9e87b91d968
Laenor Sama <LaenorSama@yandex.ru>
2022-10-10 00:04:22 +0500

    Initial commit

---
+++ variables.py
@@ -0,0 +1,20 @@
+# описание характеристик скаковой лошади / description of the characteristics of a racehorse
+
+# кличка / name
+# str
+name = 'Black Flash'
+
+# возраст (лет) / age (years)
+# float
+age = 6.6
+
+# количество побед / number of wins
+# int
+wins = 10
+
+# еще учавствует в соревнованиях (выступает) / still participates in competitions (performs)
+# boolean
+is_performs = True
+
+# выведем все характеристики и типы данных / we will output all the characteristics and data types
+print(name, type(name),age,type(age),wins,type(wins),is_performs,type(is_performs))
